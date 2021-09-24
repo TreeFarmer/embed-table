ebd945c928832181dc91d09c4196975c20890f56
Fyrlex <fyrlex@gmail.com>
2021-09-24 18:36:30 -0400

    added jsdoc to types

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,8 +1,34 @@
 export interface TableData {
+  /**
+   * An array of titles for the Table
+   */
   titles: string[],
+  /**
+   * The starting indexes for each title
+   */
   titleIndexes: number[],
+  /**
+   * The starting indexes for each column of data
+   */
   rowIndexes: number[],
+  /**
+   * A string to add to the beginning of every row
+   */
   start?: string
+  /**
+   * A string to add to the end of every row
+   */
   end?: string
+  /**
+   * A pad for the end of each row, before the end
+   * @see [String.prototype.padEnd()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd)
+   */
   padEnd?: number
+}
+
+export interface RowOptionData {
+  /**
+   * Override the table's default padEnd in the row
+   */
+  override: number
 }
