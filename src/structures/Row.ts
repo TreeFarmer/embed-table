7aebd50daf91ca75db50556ef533621d3f012827
João Pedro Almeida <33964846+Touratica@users.noreply.github.com>
2022-11-29 05:34:44 +0000

    Fix package level issues (#2)
    Fix necessity for globally installed typescript
    Add support for require() imports
    
    Fixes #5
    Fixes #6

--- src/structures/Row.ts
+++ src/structures/Row.ts
@@ -1,49 +1,59 @@
-import { RowData } from 'index.js';
+import { RowData } from "../typings/index.js";
 
 export class Row {
-	/**
-	 * The data to insert for each column
-	 */
-	private readonly columns: string[];
+  /**
+   * The data to insert for each column
+   */
+  private readonly columns: string[];
 
-	/**
-	 * The starting indexes of each column
-	 */
-	private readonly indexes: number[];
+  /**
+   * The starting indexes of each column
+   */
+  private readonly indexes: number[];
 
-	/**
-	 * Whether or not to include the Whitespace character (\u200b) in spacing (not required if using backticks for the start and end)
-	 */
-	private whiteSpace: boolean;
+  /**
+   * Whether or not to include the Whitespace character (\u200b) in spacing (not required if using backticks for the start and end)
+   */
+  private whiteSpace: boolean;
 
-	/**
-	 * Create a new Row to be inserted into a table
-	 * @param {RowData} options 
-	 */
-	public constructor(options: RowData) {
-		this.columns = options.columns;
-		this.indexes = options.indexes;
-		this.whiteSpace = options.whiteSpace;
-	}
+  /**
+   * Create a new Row to be inserted into a table
+   * @param {RowData} options
+   */
+  public constructor(options: RowData) {
+    this.columns = options.columns;
+    this.indexes = options.indexes;
+    this.whiteSpace = options.whiteSpace;
+  }
 
-	/**
-	 * Convert the row into a string
-	 * @returns {string}
-	 */
-	public toString(): string {
-		let res = '';
+  /**
+   * Convert the row into a string
+   * @returns {string}
+   */
+  public toString(): string {
+    let res = "";
 
-		for (let i = 0; i < this.columns.length; i++) res += this.padRow(i);
+    for (let i = 0; i < this.columns.length; i++) res += this.padRow(i);
 
-		return res;
-	}
+    return res;
+  }
 
-	/**
-	 * Adds the proper spacing to the data in the row
-	 * @param {number} i 
-	 * @returns {string}
-	 */
-	private padRow(i: number): string {
-		return `${this.whiteSpace ? '\u200b ' : ' '}`.repeat(this.indexes[i]! - (this.indexes[i - 1]! ?? 0) - (this.columns[i - 1] ? (this.columns[i - 1] + '').length : 0)) + this.columns[i]!.slice(0, (this.indexes[i + 1] ?? Infinity) - this.indexes[i]!);
-	}
+  /**
+   * Adds the proper spacing to the data in the row
+   * @param {number} i
+   * @returns {string}
+   */
+  private padRow(i: number): string {
+    return (
+      `${this.whiteSpace ? "\u200b " : " "}`.repeat(
+        this.indexes[i]! -
+          (this.indexes[i - 1]! ?? 0) -
+          (this.columns[i - 1] ? (this.columns[i - 1] + "").length : 0)
+      ) +
+      this.columns[i]!.slice(
+        0,
+        (this.indexes[i + 1] ?? Infinity) - this.indexes[i]!
+      )
+    );
+  }
 }
